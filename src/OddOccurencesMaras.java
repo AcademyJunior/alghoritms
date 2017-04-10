2d387a614a9fe97e5fea7d2154d193616e1279d3
Marek <marek.zemb@gmaik.com>
2017-04-11 00:21:56 +0100

    klasa z rozwiazaniem occurences

---
+++ src/OddOccurencesMaras.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class OddOccurencesMaras {
+
+    public static int solution(int a[], int n) {
+        int number;
+        Arrays.sort(a);
+        for (int i = 0; i < a.length-2; i++) {
+            if (a[i] != a[i + 1]) {
+                number = a[i];
+                return number;
+            } else {
+               i++;
+            }
+        }
+        number = a[a.length-1];
+        return number;
+    }
+
+
+}
--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,8 @@
 public class Start {
     public static void main(String[] args) {
         int n = 1200;
-        System.out.println(BinaryGapMaras.solution(n));
+        int[] numbers = {1, 3, 5, 4, 5, 1, 5, 3, 5};
+        System.out.println(OddOccurencesMaras.solution(numbers, 9));
         }
 
 }
