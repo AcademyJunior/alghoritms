ffccef36f41152e258a7c0a6e9b8341fcfffe330
Marek <marek.zemb@gmaik.com>
2017-04-12 12:49:10 +0100

    new files

---
+++ src/ArrayRotationMaras.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ArrayRotationMaras {
+
+    public static int[] solution(int[] A, int K) {
+        if(K == 0){return A;}
+        int actualRotation = K % (A.length);
+        int digitsToMove = A.length - actualRotation;
+        List<Integer> list = IntStream.of(A).boxed().collect(Collectors.toList());
+        for (int x = 0; x < digitsToMove; x++) {
+            list.add(list.get(x));
+        }
+    return list.stream().skip(digitsToMove).mapToInt(i->i).toArray();
+    }
+}
--- src/Start.java
+++ src/Start.java
@@ -2,9 +2,12 @@
 public class Start {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 3, 5, 4, 5, 1, 5, 3, 5};
-        int n = numbers.length-1;
-        System.out.println(OddOccurencesMaras.solution(numbers));
+        int[] numbers = {1, 3, 5, 7, 3, 5, 6, 1, 12, 4, 7, 20, 10};
+
+        for(int x: ArrayRotationMaras.solution(numbers, 3)){
+            System.out.println(x);
+        }
+
         }
 
 }
