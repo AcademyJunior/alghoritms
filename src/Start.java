8578c65af29484768c5012702c65eeedaccf3c95
Marek <marek.zemb@gmaik.com>
2017-04-12 18:25:04 +0100

    added files

--- src/Start.java
+++ src/Start.java
@@ -2,11 +2,9 @@
 public class Start {
     public static void main(String[] args) {
 
-        int[] numbers = {1, 3, 5, 7, 3, 5, 6, 1, 12, 4, 7, 20, 10};
+        int[] numbers = {2, 3, 1, 5};
 
-        for(int x: ArrayRotationMaras.solution(numbers, 3)){
-            System.out.println(x);
-        }
+        System.out.println(PermMissingMaras.solution(numbers));
 
         }
 
