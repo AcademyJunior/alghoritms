7a2dfbf5fedb386423ed9344d4a077796b3d02d6
lukozog <lukaszozog2@gmail.com>
2017-04-13 20:31:23 +0200

    true commit

--- src/PermMissingOrzech.java
+++ src/PermMissingOrzech.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-/**
- * Created by Łukasz on 2017-04-13.
- */
 public class PermMissingOrzech {
+    int solution(int [] A) {
+        LinkedList<Integer> listA=new LinkedList<>();
+        for(int i:A){
+            listA.add(i);
+        }
+        Collections.sort(listA);
+        Integer missingElement = 1;
+        for (Integer i : listA) {
+            if (!missingElement.equals(i)) {
+                break;
+            } else
+                missingElement++;
+        }
+        return missingElement;
+    }
 }
--- src/Start.java
+++ src/Start.java
@@ -1,6 +1,9 @@
 
 public class Start {
     public static void main(String[] args) {
-
+        PermMissingOrzech pmo=new PermMissingOrzech();
+        //int [] array=new int[]{2,3,8,4,5,1,6};
+        int [] array=new int[]{1,2,3};
+        System.out.println(pmo.solution(array));
     }
 }
