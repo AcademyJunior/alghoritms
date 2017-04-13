962dd8785e065a2361208fa8c194c490eb4845dd
lukozog <lukaszozog2@gmail.com>
2017-04-13 18:40:57 +0200

    refactor

--- src/BinaryGapOrzech.java
+++ src/BinaryGapOrzech.java
@@ -1,24 +1,18 @@
-/**
- * Created by Łukasz on 2017-04-09.
- */
 public class BinaryGapOrzech {
-    public int solutionOrzech(int N){
-        String nat=Integer.toBinaryString(N);
-        List<String> myList = new ArrayList<String>(Arrays.asList(nat.split("")));
-        int binaryGap=0;
-        int biggestGap=0;
+    public int solutionOrzech(int N) {
+        String binaryAsString = Integer.toBinaryString(N);
+        int binaryGap = 0;
+        int biggestGap = 0;
 
-        for(int i=0;i<myList.size() && myList.size()!=0;i++){
-            if(myList.get(i).equals("0")){
+        for (int i = 0; i < binaryAsString.length(); i++) {
+            if (binaryAsString.charAt(i) == '0') {
                 binaryGap++;
             }
-            else{
-                if(binaryGap>0 && binaryGap>biggestGap){
-                    biggestGap=binaryGap;
-                    binaryGap=0;
+            else {
+                if (binaryGap > 0 && binaryGap > biggestGap) {
+                    biggestGap = binaryGap;
                 }
-                else
-                    binaryGap=0;
+                binaryGap = 0;
             }
         }
         return biggestGap;
