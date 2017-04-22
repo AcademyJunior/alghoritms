799f4d0cad4c37783b4b42c8f920830e35792b5b
lukozog <lukaszozog2@gmail.com>
2017-04-22 08:43:34 +0200

    added annotation

--- src/MissingIntegerTest.java
+++ src/MissingIntegerTest.java
@@ -7,11 +7,12 @@ public class MissingIntegerTest {
         int[]array={-1,-2,5,-200,1,4,-7,3,2,6,7};
         assertEquals("Mixed sauce? Yes please ( ͡° ͜ʖ ͡°)",8,new MissingInteger().solution(array));
     }
+    @org.junit.Test
     public void testNegative(){
         int []array={-1000,-500,-3,-501,-1};
         assertEquals("Test of negative numbers",1,new MissingInteger().solution(array));
     }
-
+    @org.junit.Test
     public void testPositive(){
         int[] array={1,3,10,239239,4,2,5,8};
         assertEquals("Test of positive numbers",6,new MissingInteger().solution(array));
