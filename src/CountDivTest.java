ee308ddebe20e46fe43ca709150900b9460a07c8
lukozog <lukaszozog2@gmail.com>
2017-04-26 17:31:44 +0200

    cleaning

--- src/CountDivTest.java
+++ src/CountDivTest.java
@@ -7,9 +7,6 @@ public class CountDivTest {
     @Test
     public void testCodility(){
         assertEquals("Codility test",1,new CountDiv().solution(11,13,2));
-        assertEquals("Codility test",19,new CountDiv().solution(18,345,17));
-        assertEquals("Codility test",3,new CountDiv().solution(6,11,2));
-        assertEquals("Codility test",2,new CountDiv().solution(0,200000,200000));
     }
 
     @Test
